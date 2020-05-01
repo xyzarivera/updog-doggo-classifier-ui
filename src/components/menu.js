dacb57831e8f97f9542cba851919b062bcb24926
Xyza Rivera <xyzarivera@gmail.com>
2020-05-01 19:12:32 +0800

    added application title

--- src/components/menu.js
+++ src/components/menu.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 
 export default () => (
   <nav>
+    <Link to="/">Doggo Classifier</Link>
     <Link to="/">Home</Link>
     <Link to="/about/">About</Link>
   </nav>

3aa0ca8cee7d195bf57c48d3f92dbb7bfeff8065
faustroxas <63102171+faustroxas@users.noreply.github.com>
2020-05-07 04:20:47 +0800

    Temporarily removed API key

--- src/services/uploadImage.js
+++ src/services/uploadImage.js
@@ -5,18 +5,6 @@
   var firebase = require("firebase/app");
   var storage = require("firebase/storage");
 
-  // Initialize Firebase App
-  const firebaseConfig = {
-    apiKey: "AIzaSyDQYWlHs1FCvUR4K6PWlhbEX4Q3baUG_ds",
-    authDomain: "gcp-revalida-a.firebaseapp.com",
-    databaseURL: "https://gcp-revalida-a.firebaseio.com",
-    projectId: "gcp-revalida-a",
-    storageBucket: "gcp-revalida-a.appspot.com",
-    messagingSenderId: "736552925421",
-    appId: "1:736552925421:web:1a4a2d1b318ad46b4874e4",
-    measurementId: "G-Q2CCPPDBKH"
-  };
-  firebase.initializeApp(firebaseConfig);
   //--------------------------------------------------------------------------------------
 
   // Replace with Loading bar id

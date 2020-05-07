3c710caab994235f76d1ca470919c827ce4bd25d
Xyza Rivera <xyzarivera@gmail.com>
2020-05-08 06:55:07 +0800

    edited styling

--- src/containers/app.js
+++ src/containers/app.js
@@ -69,6 +69,7 @@ class App extends Component {
                     score:data[0].score,
                     breed:data[0].breed
                     })
+                console.log(data[0].score)
                 }  
             )
             .catch(err => {
@@ -78,10 +79,18 @@ class App extends Component {
     } 
 
     render() {
+        const score = this.state.score;
+        const breed = this.state.breed;
+        let header = "";
+
+        if (breed!="") {
+            header = "I'm " + score + "% sure that you're a " + breed + " !"
+        }
+
         return (
             <div class="bg-color-whiteish add-padding-top flex w-full h-screen text-center items-center">
             <Layout>
-                <div class="flex flex-wrap px-8 mb-6">
+                <div class="flex flex-wrap px-2 mb-6">
                     <div class="flex justify-center md:w-1/3 px-4">
                         <img class="logo-size" alt='UPDOG' src={logo} />
                     </div>
@@ -92,37 +101,47 @@ class App extends Component {
                         </p>
                         <p class="text-gray-700">
                             <b>UPDOG</b> is a web-based computer vision application that identifies the breed of a dog in an uploaded photo.
-                            <p class="text-doggored">under development</p>
+{/*                           <p class="text-doggored">under development</p>
+*/} 
                         </p>
                     </div>
                 </div>
-                <form class="px-6 py-4">
+                <form class="px-6 py-2 justify-center">
                     <div class="bg-color-whiteish-darker rounded-corners py-4">
-                        <div>
-                            <div></div>
-                            <label class="block text-gray-500 font-bold">
-                                <span class="text-sm">
-                                    Upload a doggo selfie!
-                                </span>
-                            </label>
-                        </div>
-                        <div class="flex items-center justify-center bg-grey-lighter">
-                            <img src={this.state.url || "http://via.placeholder.com/300"} alt="uploaded photo" />
-                            <label class="w-56 flex flex-col items-center px-2 py-4 bg-white text-gray rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-gray-200">
-                                <svg class="w-6 h-6 mobileHide" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
-                                </svg>
-                                <span class="text-base leading-normal">Select a photo</span>
-                                <input type='file' class="hidden" onChange={this.handleUpload} />
-                            </label>
-                        </div>
-                    </div>
-                    <div>
-                        <div></div>
-                        <div>
-                        <button onClick={this.handleData} class="shadow bg-doggored focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
-                            BOOP!
-                        </button>
+                        <div class="flex items-center justify-center bg-grey-lighter mb-6">
+                            <div class="flex md:w-3/6 justify-center">
+                                <img src={this.state.url} alt="" />
+                            </div>
+                            <div class="md:w-3/6">
+                                <div class="justify-center">
+                                    <label class="block text-gray-500 font-bold text-doggored">
+                                        <span class="text-xl">
+                                            {header}
+                                        </span>
+                                    </label>
+                                </div>
+                                <div class="justify-center">
+                                <label class="block text-gray-500 font-bold">
+                                    <span class="text-sm">
+                                        Upload a doggo selfie!
+                                    </span>
+                                </label>
+                            </div>
+                                <div class="flex items-center justify-center py-2">
+                                    <label class="w-48 flex justify-center items-center flex-col items-center px-2 py-4 bg-white text-gray rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-gray-200">
+                                        <svg class="w-6 h-6 mobileHide" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
+                                        </svg>
+                                        <span class="text-base leading-normal">Select a photo</span>
+                                        <input type='file' class="hidden" onChange={this.handleUpload} />
+                                    </label>
+                                </div>
+                                <button onClick={this.handleData} class="shadow bg-doggored focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                                    BOOP for me to guess!
+                                </button>
+                            </div>
+                            <div>
+                            </div>
                         </div>
                     </div>
                 </form>

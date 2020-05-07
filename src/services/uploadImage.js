8ea17607e9ca6aa08f5e75857c7723e7868e0cc8
faustroxas <randito.roxasjr@gmail.com>
2020-05-07 11:03:48 +0800

    Unexposes API keys and uses reserved URLs: Please see move_to_body.html

--- src/services/uploadImage.js
+++ src/services/uploadImage.js
@@ -1,53 +1,48 @@
-// PLEASE MOVE the <script></script> to the <body></body> of the web app
 // Better to use EventListener instead of Form submission 
-<script src="/__/firebase/7.14.2/firebase-storage.js">
 
-  var firebase = require("firebase/app");
-  var storage = require("firebase/storage");
+var firebase = require("firebase/app");
+var storage = require("firebase/storage");
 
-  // Initialize Firebase App
-  const firebaseConfig = {
-    apiKey: "AIzaSyDQYWlHs1FCvUR4K6PWlhbEX4Q3baUG_ds",
-    authDomain: "gcp-revalida-a.firebaseapp.com",
-    databaseURL: "https://gcp-revalida-a.firebaseio.com",
-    projectId: "gcp-revalida-a",
-    storageBucket: "gcp-revalida-a.appspot.com",
-    messagingSenderId: "736552925421",
-    appId: "1:736552925421:web:1a4a2d1b318ad46b4874e4",
-    measurementId: "G-Q2CCPPDBKH"
-  };
-  firebase.initializeApp(firebaseConfig);
-  //--------------------------------------------------------------------------------------
+// Initialize Firebase App
+const firebaseConfig = {
+  apiKey: "AIzaSyDQYWlHs1FCvUR4K6PWlhbEX4Q3baUG_ds",
+  authDomain: "gcp-revalida-a.firebaseapp.com",
+  databaseURL: "https://gcp-revalida-a.firebaseio.com",
+  projectId: "gcp-revalida-a",
+  storageBucket: "gcp-revalida-a.appspot.com",
+  messagingSenderId: "736552925421",
+  appId: "1:736552925421:web:1a4a2d1b318ad46b4874e4",
+  measurementId: "G-Q2CCPPDBKH"
+};
+firebase.initializeApp(firebaseConfig);
+//--------------------------------------------------------------------------------------
 
-  // Replace with Loading bar id
-  var uploader = document.getElementById('uploader');
+// Replace with Loading bar id
+var uploader = document.getElementById('uploader');
 
-  // Replace Selector with ID of <input type="file">
-  var fileButton = document.getElementById('fileButton');
+// Replace Selector with ID of <input type="file">
+var fileButton = document.getElementById('fileButton');
 
-  // Wait for value to change in input
-  fileButton.addEventListener('change', function(e){
-    var file = e.target.files[0];
-    //var file = document.getElementById('image');
+// Wait for value to change in input
+fileButton.addEventListener('change', function(e){
+  var file = e.target.files[0];
 
-    // Name and Upload file
-    var storageRef = firebase.storage().ref('img/'+file.name); 
-    
-    var uploadTask = storageRef.put(file);
-    
-    // Listen for state changes, errors, and completion of the upload.
-    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-      function(snapshot) {
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        uploader.value = progress;
-        console.log('Upload is ' + progress + '% done');
-      }, function() {
-      // Upload completed successfully, now we can get the download URL
-      uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-        console.log('File available at', downloadURL);
-      });
+  // Name and Upload file
+  var storageRef = firebase.storage().ref('img/'+file.name); 
+  
+  var uploadTask = storageRef.put(file);
+  
+  // Listen for state changes, errors, and completion of the upload.
+  uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+    function(snapshot) {
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      uploader.value = progress;
+      console.log('Upload is ' + progress + '% done');
+    }, function() {
+    // Upload completed successfully, now we can get the download URL
+    uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+      console.log('File available at', downloadURL);
     });
   });
-
-</script>
+});
